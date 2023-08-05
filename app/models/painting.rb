922619fda4cbd42ac00b44cb088b890220a95029
noia1111 <132414908+noia1111@users.noreply.github.com>
2023-08-05 14:51:54 +0000

    [画像の詳細ページでの表示変更]

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -24,6 +24,13 @@ class Painting < ApplicationRecord
     end
     image.variant(resize: "#{width}x#{height}!").processed
   end
+  def get_image2
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_header_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
   # 検索オプション
   def self.looks(word)
       @painting = Painting.where("title LIKE?","%#{word}%")
